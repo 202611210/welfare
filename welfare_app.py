b234f7c911de0dbfb1eb5876d8f8458fac13951d
jimih <2026_11210@kshcm.net>
2026-06-14 01:01:29 +0700

    Update welfare_app.py

--- welfare_app.py
+++ welfare_app.py
@@ -4,7 +4,6 @@ import numpy as np
 import joblib
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-import platform
 import matplotlib.font_manager as fm
 
 # ────────────────────────────────────────────
